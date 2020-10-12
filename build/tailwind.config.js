e1fd2fc92757af1837c3cd21a1ad2eb2ed02877a
Tomos <Willistomos@gmail.com>
2020-10-12 11:57:53 +0100

    First Commit - baseplate

---
+++ build/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	purge: false,
+	target: 'relaxed',
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	corePlugins: {},
+	future: {
+		removeDeprecatedGapUtilities: true,
+	},
+};
---
+++ resources/assets/js/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-new */
+
+import Vue from 'vue';
+
+// Common
+// import Foo from './components/Foo';
+// import Bar from './components/Bar';
+
+// Global
+// Vue.component('Foo', Foo);
+
+new Vue({
+	el: '#app',
+
+	components: {
+		// Bar,
+	},
+});
